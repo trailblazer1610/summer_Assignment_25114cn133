eb7dd93be92fe2a3bf236b60370264acaa7912d4
Kuldeepak Vishwakarma <98377229+trailblazer1610@users.noreply.github.com>
2026-05-28 19:37:18 +0530

    table day_1/_2.c

--- day_1/_2.c
+++ day_1/_2.c
@@ -1,13 +1,18 @@
 #include <stdio.h>
 int main(){
 
-int 
+int n,i,m;
+printf("enter the number: ");
+scanf("%d",&n);
+printf("enter the number of term : ");
+scanf("%d",&m);
+printf("\n");
+//print table
+printf(" table of %d is : \n",n);
+for(i=1;i<n;i++){
 
-
-
-
-
-
+    printf("%d * %d = %d\n",n,i,n*i);
+}
 
     return 0;
 }
