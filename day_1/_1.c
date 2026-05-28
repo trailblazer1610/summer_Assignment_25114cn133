ee3d2bd96da8f6f4a2f29e5ca7717e3a7738fc2b
Kuldeepak Vishwakarma <98377229+trailblazer1610@users.noreply.github.com>
2026-05-28 19:04:45 +0530

    new commit

--- day_1/_1.c
+++ day_1/_1.c
@@ -2,15 +2,12 @@
 
 int main() {
     int n,i,sum=0;
-
     printf("Enter a positive integer: ");
     scanf("%d", &n);
 
     for (i=1;i<=n;++i) {
         sum=sum+i ;
     }
-
     printf("Sum of first %d natural numbers is %d\n", n, sum);
-
     return 0;
 }
