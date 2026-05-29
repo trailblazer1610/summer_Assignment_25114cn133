95b8aebb8dfd11729f965aac2ad53727c663e029
Kuldeepak Vishwakarma <98377229+trailblazer1610@users.noreply.github.com>
2026-05-29 13:36:37 +0530

    day 3

--- day_3/_9.c
+++ day_3/_9.c
@@ -1,22 +1,22 @@
 #include <stdio.h>
 
-int prime(int n){
+void prime(int n){
     int i;
-    for(i=2;i<=n/2;i++)
-        if(n/i==0)
-        return 0;
-        else
-        return 1;
-    
+    if(n==0||n==1)
+    return printf("number is not a prime");
+
+    for(i=2;i<=n/2;i++){
+       if(n%i==0){
+         return printf("number is not a prime");
+       }
+    }
+    printf("number is a prime");
 }
 
 int main() {
     int n;
     printf(" enter the number :");
     scanf("%d",&n);
-    if(prime(n)==1)
-    printf("number is not prime");
-    else
-    printf("number is prime");
+    prime(n);
     return 0;
 }
