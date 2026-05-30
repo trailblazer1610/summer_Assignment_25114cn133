a660b59d9dc2d7c71ebec5305202ff3994f084f6
Kuldeepak Vishwakarma <98377229+trailblazer1610@users.noreply.github.com>
2026-05-30 19:40:24 +0530

    done

---
+++ day_3/_12.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+
+// Function to find GCD using Euclidean algorithm
+int find_gcd(int a, int b) {
+    while (b != 0) {
+        int temp = b;
+        b = a % b;
+        a = temp;
+    }
+    return a;
+}
+
+int main() {
+    int num1, num2, gcd, lcm;
+
+    printf("Enter two positive integers: ");
+    scanf("%d %d", &num1, &num2);
+
+    // Calculate GCD first
+    gcd = find_gcd(num1, num2);
+
+    // Calculate LCM using the formula
+    lcm = (num1 * num2) / gcd;
+
+    printf("The LCM of %d and %d is %d\n", num1, num2, lcm);
+
+    return 0;
+}
