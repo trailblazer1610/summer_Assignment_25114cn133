3b5fb90802df5f67df1a6eac8f66d31de5d0f699
Kuldeepak Vishwakarma <98377229+trailblazer1610@users.noreply.github.com>
2026-06-05 01:01:34 +0530

    pattern

---
+++ pattern practice/_1.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+int main() {
+    
+int i,j,n;
+
+printf("enter the number :");
+scanf("%d",&n);
+printf("\n");
+
+for(i=0;i<=n;i++){
+    for(j=0;j<i;j++){
+
+        printf("* ");
+    }
+    printf("\n");
+}
+ return 0;
+}
