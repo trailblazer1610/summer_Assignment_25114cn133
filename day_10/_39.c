3a8cc0d0f7a50ed1d689bc25af2fa6ab95fecca5
Kuldeepak Vishwakarma <98377229+trailblazer1610@users.noreply.github.com>
2026-06-05 13:11:50 +0530

    done

--- day_10/_39.c
+++ day_10/_39.c
@@ -10,20 +10,26 @@
 int main() {
     
 int i,j,k,n;
+int num;
 
 printf("enter the number :");
 scanf("%d",&n);
-printf("\n");
-
 
 for(i=0;i<=n;i++){
     for(j=0;j<n-i;j++){
-        printf("  ");
+        printf(" ");
     }
-    for(k=0;k<2*i-1;k++){
-        printf("* ");
+    for(k=0;k<(2*i-1)/2;k++){
+        printf("%d",num++);
     }
+
+     for(k=0;k<(2*i)/2;k++){
+        printf("%d",num--);
+     }
+
+
 printf("\n");
+num=1;
 }
     return 0;
 }
--- day_10/_40.c
+++ day_10/_40.c
@@ -9,21 +9,27 @@
 
 int main() {
     
-int i,j,k,n,l;
+int i,j,k,n;
+char ch='A';
 
 printf("enter the number :");
 scanf("%d",&n);
-printf("\n");
-
 
 for(i=0;i<=n;i++){
     for(j=0;j<n-i;j++){
-        printf("  ");
+        printf(" ");
     }
-    for(k=0;k<2*i-1;k++){
-        printf("* ");
+    for(k=0;k<(2*i-1)/2;k++){
+        printf("%c",ch++);
     }
+
+     for(k=0;k<(2*i)/2;k++){
+        printf("%c",ch--);
+     }
+
+
 printf("\n");
+ch='A';
 }
     return 0;
 }
