058767b8fac96473a8b1f01ab255e63ff8b9dbe8
Kuldeepak Vishwakarma <98377229+trailblazer1610@users.noreply.github.com>
2026-06-17 19:37:42 +0530

    kv

--- day_13/_51.c
+++ day_13/_51.c
@@ -4,9 +4,33 @@
 
 int main() {
     
+int arr[50];
+int i,n;
+int key;
 
+printf("enter the no. of term of array :");
+scanf("%d",&n);
 
+for(i=0;i<n;i++)
 
-    
-    return 0;
+{
+    printf("enter the %d term of array :",i+1);
+    scanf("%d",&arr[i]);
+}
+
+key = arr[0];
+for(i=0;i<n;i++)
+{if(key<arr[i])
+key=arr[i];
+}
+printf("largest element is :%d\n",key);
+
+key = arr[0];
+for(i=0;i<n;i++)
+{if(key>arr[i])
+key=arr[i];
+}
+printf("smallest element is :%d",key);
+
+return 0;
 }
